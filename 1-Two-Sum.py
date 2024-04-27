6e0ffbaece95c4c19e7c24a5e19df03a3ec156ae
Fepipe <felipeepilefcv12@gmail.com>
2024-04-27 01:14:49 -0300

    Create 1- Two Sum

---
+++ 1-Two-Sum.py
@@ -0,0 +1,15 @@
+def twoSum(nums, target):
+    hash = {}
+
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in hash:
+            return [hash[diff], i]
+        hash[n] = i
+
+
+nums = [10,2,3,5]
+target = 7
+
+result = twoSum(nums, target)
+print(result)
